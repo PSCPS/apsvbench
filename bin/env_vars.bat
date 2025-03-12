ffb32b48b9c242137ffcfb48d55a648e7b1d7641
SteveSouthwell <ssouthwe@progress.com>
2025-03-12 14:24:10 -0500

    Initial checkin

---
+++ bin/env_vars.bat
@@ -0,0 +1,7 @@
+:: Set the location where you've installed apsvbench
+set APSVBENCH=C:\apps\apsvbench
+
+:: Set your APSV connection string.
+:: example:
+set APSVCONNECTSTRING=-URL http://myserver:8810/myapp/apsv -sessionModel Session-free
+
