3bfd4239767ee3b8f22fa422a415028fe4b1ae32
SteveSouthwell <ssouthwe@progress.com>
2025-07-16 11:49:36 -0500

    Added new vars related to discarding outliers and warmup runs

--- bin/env_vars.bat
+++ bin/env_vars.bat
@@ -1,10 +1,18 @@
 :: Set the location where you've installed apsvbench
 set APSVBENCH=C:\apps\apsvbench
 
+:: Discarding results
+set DISCARDOUTLIERS=TRUE
+::set DISCARDTYPE=FIXED:2
+::set DISCARDTYPE=PERCENT:5
+set DISCARDTYPE=IQR:1.5
+:: DISCARDTYPE: FIXED=Set number of records from either end to remove.  IQR: Remove numbers below Q1 - x*IQR or above Q3 + x*IQR
+set WARMUPRUNS=1
+
 :: Set your APSV connection string.
 :: example:
-set APSVCONNECTSTRING=-URL https://myserver.com/myapp/apsv -sessionModel Session-free 
-
+set APSVCONNECTSTRING=-URL https://myserver.com/myapp/apsv -sessionModel Session-free
+ 
 :: Number of seconds of server-side stats to grab at the end of the test
 set SERVERSTATSSECONDS=30
 
@@ -14,8 +22,8 @@ set ABLAPPNAME=myapp
 :: Histogram control
 set HIST_SHOWHISTOGRAM=TRUE
 ::    Number includes out-of-range buckets where value is below lowrange or above high range
-set HIST_NUMBUCKETS=15
-
+set HIST_NUMBUCKETS=10
+::    How to scale the size of each histogram bar.  Only option right now is "auto".
 set HIST_BARSCALE=auto
 
 :: Histogram range type:  
@@ -23,5 +31,5 @@ set HIST_BARSCALE=auto
 ::    fixed = even divisions starting at low range, by bucket size.
 set HIST_RANGETYPE=auto
 ::set HIST_RANGETYPE=fixed
-::set HIST_LOWRANGE=0.51
-::set HIST_BUCKETSIZE=0.015
+::set HIST_LOWRANGE=0.53
+::set HIST_BUCKETSIZE=0.0025
